ecf8f7723b10d702685ddd5a744d8dc83a7a366a
RAHUL SANGRAL <rahul.rishusangral@gmail.com>
2026-04-11 01:21:51 +0530

    final fix

--- server/app.py
+++ server/app.py
@@ -109,7 +109,7 @@ def get_history():
 
 def main():
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
     main()
